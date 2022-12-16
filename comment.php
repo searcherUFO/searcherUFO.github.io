528b5cacf0ef6e6b20eb6e23435942cf51b84423
searcherUFO <120590031+searcherUFO@users.noreply.github.com>
2022-12-16 09:51:51 +0100

    Update comment.php

--- comment.php
+++ comment.php
@@ -1,26 +1,4 @@
-<?php
- 
- if($_POST['Submit']){
-  print "<h1>Your comment has been submitted!</h1>";
-
-  $Name = $_POST['Name'];
-  $Comment = $_POST['Comment'];
-
-  #Get old comments
-  $old = fopen("comments.html", "r+t");
-  $old_comments = fread($old, 1024);
-
-  #Delete everything, write down new and old comments
-  $write = fopen("comments.html", "w+");
-  $string = "<b>".$Name."</b><br>".$Comment."<br>\n".$old_comments;
-  fwrite($write, $string);
-  fclose($write);
-  fclose($old);
- }
-
- #Read comments
- $read = fopen("comments.html", "r+t");
- echo "<br><br>Comments<hr>".fread($read, 1024);
- fclose($read);
-
-?>
+CREATE TABLE comments (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  comment VARCHAR(255) NOT NULL
+);
