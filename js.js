53c4dbe39dc165ade5830aa4e999d44e7fae5da7
searcherUFO <120590031+searcherUFO@users.noreply.github.com>
2022-12-17 18:16:45 +0100

    Create js.js

---
+++ js.js
@@ -0,0 +1,6 @@
+var searchBox = document.getElementById("searchBox");
+var googleIcon = document.getElementById("googleIcon");
+
+googleIcon.onclick = function(){
+    searchBox.classList.toggle("active");
+}
