96b1bee17e1c919a8e71ba5277ea5359ec5c89d4
searcherUFO <120590031+searcherUFO@users.noreply.github.com>
2022-12-17 12:22:05 +0100

    Create commentbox.php

---
+++ commentbox.php
@@ -0,0 +1,26 @@
+<?php
+ 
+ if($_POST['Submit']){
+  print "<h1>Your comment has been submitted!</h1>";
+
+  $Name = $_POST['Name'];
+  $Comment = $_POST['Comment'];
+
+  #Get old comments
+  $old = fopen("comments.html", "r+t");
+  $old_comments = fread($old, 1024);
+
+  #Delete everything, write down new and old comments
+  $write = fopen("comments.html", "w+");
+  $string = "<b>".$Name."</b><br>".$Comment."<br>\n".$old_comments;
+  fwrite($write, $string);
+  fclose($write);
+  fclose($old);
+ }
+
+ #Read comments
+ $read = fopen("comments.html", "r+t");
+ echo "<br><br>Comments<hr>".fread($read, 1024);
+ fclose($read);
+
+?>
