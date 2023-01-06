aa6a99e517543ab3cff70b856da68820d06d3538
searcherUFO <120590031+searcherUFO@users.noreply.github.com>
2023-01-06 20:22:15 +0100

    Delete js.js

--- js.js
+++
@@ -1,6 +0,0 @@
-var searchBox = document.getElementById("searchBox");
-var googleIcon = document.getElementById("googleIcon");
-
-googleIcon.onclick = function(){
-    searchBox.classList.toggle("active");
-}
